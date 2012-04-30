9b4a2820c0e4ee9f4dae046c7fb47a9f5feff4b4
musicboy <m.boy@live.cn>
2012-04-30 17:50:55 +0800

    Update the areatrigger_teleport for opening from Molten core

---
+++ sql/musicboy/2012_02_04_molten_core_oeffnung.sql
@@ -0,0 +1,6 @@
+-- Am 05.02.2012 erst einspielen!!!
+
+UPDATE `areatrigger_teleport` SET `id` = '2886' WHERE `name` = 'The Molten Bridge';
+UPDATE `areatrigger_teleport` SET `id` = '2890' WHERE `name` = 'Molten Core Entrance, Inside';
+UPDATE `areatrigger_teleport` SET `id` = '3528' WHERE `id` = '9990';
+UPDATE `areatrigger_teleport` SET `id` = '3529' WHERE `id` = '9989';
